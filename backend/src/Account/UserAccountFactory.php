420e426d169efc0cf0475ed194c75a7dfadc0199
Nikolay Mikhaylov <sonny@milton.pro>
2020-12-13 02:09:24 +0300

    Use transaction in account factory

--- backend/src/Account/UserAccountFactory.php
+++ backend/src/Account/UserAccountFactory.php
@@ -10,13 +10,35 @@
 namespace App\Account;
 
 use App\Entity\Account\AbstractAccount;
+use App\Entity\Account\ServiceAccount;
 use App\Entity\Account\UserAccount;
+use App\Repository\Account\ServiceAccountRepository;
+use App\Transaction\TransactionManager;
+use Doctrine\ORM\EntityManagerInterface;
 
 /**
  * User account factory
  */
 class UserAccountFactory implements AccountFactoryInterface
 {
+    private const WELCOME_BONUS_AMOUNT = 500;
+
+    private EntityManagerInterface $em;
+
+    private TransactionManager $transactionManager;
+
+    /**
+     * UserAccountFactory constructor.
+     *
+     * @param \Doctrine\ORM\EntityManagerInterface $em
+     * @param \App\Transaction\TransactionManager  $transactionManager
+     */
+    public function __construct(EntityManagerInterface $em, TransactionManager $transactionManager)
+    {
+        $this->em                 = $em;
+        $this->transactionManager = $transactionManager;
+    }
+
     /**
      * @return \App\Entity\Account\AbstractAccount
      */
@@ -24,8 +46,31 @@ class UserAccountFactory implements AccountFactoryInterface
     {
         $account = new UserAccount();
 
-        $account->increaseBalance(500);
+        $this->giveWelcomeBonus($account);
 
         return $account;
+    }
+
+    /**
+     * @param \App\Entity\Account\AbstractAccount $account
+     */
+    private function giveWelcomeBonus(AbstractAccount $account): void
+    {
+        $welcomeBonusAccount = $this->getServiceAccountRepository()->findOneBy(['tag' => 'welcome_bonus']);
+
+        // bonus will not be added if service account does not exist
+        if (null === $welcomeBonusAccount) {
+            return;
+        }
+
+        $this->transactionManager->makeTransaction($welcomeBonusAccount, $account, self::WELCOME_BONUS_AMOUNT);
+    }
+
+    /**
+     * @return \App\Repository\Account\ServiceAccountRepository
+     */
+    private function getServiceAccountRepository(): ServiceAccountRepository
+    {
+        return $this->em->getRepository(ServiceAccount::class);
     }
 }
