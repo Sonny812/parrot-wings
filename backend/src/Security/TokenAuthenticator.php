59c1f303350c59001113f2238ed29e82e0b0dd63
Nikolay Mikhaylov <sonny@milton.pro>
2020-12-13 21:13:57 +0300

    Add token authenticator

---
+++ backend/src/Security/TokenAuthenticator.php
@@ -0,0 +1,111 @@
+<?php declare(strict_types=1);
+/**
+ * @author    Nickolay Mikhaylov <sonny@milton.pro>
+ * @copyright Copyright (c) 2020, Nikolay Mikhaylov
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Security;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+
+class TokenAuthenticator extends AbstractGuardAuthenticator
+{
+    private EntityManagerInterface $em;
+
+    /**
+     * TokenAuthenticator constructor.
+     *
+     * @param \Doctrine\ORM\EntityManagerInterface $em
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function start(Request $request, AuthenticationException $authException = null): Response
+    {
+        $data = [
+            'message' => 'Authentication Required',
+        ];
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function supports(Request $request): bool
+    {
+        return $request->headers->has('X-AUTH-TOKEN');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getCredentials(Request $request): string
+    {
+        return $request->headers->get('X-AUTH-TOKEN');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getUser($credentials, UserProviderInterface $userProvider): ?UserInterface
+    {
+        if (null === $credentials) {
+            return null;
+        }
+
+        return $userProvider->loadUserByUsername($credentials);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function checkCredentials($credentials, UserInterface $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
+    {
+        $data = [
+            'message' => strtr($exception->getMessageKey(), $exception->getMessageData()),
+        ];
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey): ?Response
+    {
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function supportsRememberMe(): bool
+    {
+        return false;
+    }
+}
