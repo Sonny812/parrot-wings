69f80defe74d86acc71e1efce9f0f4e4edd4092c
Nikolay Mikhaylov <sonny@milton.pro>
2020-12-14 01:26:06 +0300

    Display user name and balance in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Login from "./login";
 import jsonServerProvider from 'ra-data-json-server'
 import authProvider from './authProvider';
 import Register from './register'
+import CustomLayout from './customLayout';
 import {TransactionList, TransactionShow, TransactionCreate} from "./transaction";
 
 const httpClient = (url, options = {}) => {
@@ -22,6 +23,7 @@ const App = () => (
     <Admin
         authProvider={authProvider}
         loginPage={Login}
+        layout={CustomLayout}
         dataProvider={jsonServerProvider(process.env.REACT_APP_API_URL, httpClient)}
         customRoutes={[
             <Route exact path="/register" component={Register} noLayout/>
---
+++ frontend/src/customLayout.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {AppBar, Layout} from 'react-admin';
+import Typography from '@material-ui/core/Typography';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = {
+    title: {
+        flex: 1,
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap',
+        overflow: 'hidden',
+    },
+    spacer: {
+        flex: 1,
+    },
+};
+
+const UserData = props => {
+    const user = JSON.parse(localStorage.user);
+
+    return (
+        <div>
+            <div>{user.username}</div>
+            <div>{user.account.balance} PW</div>
+        </div>
+    )
+};
+
+const MyAppBar = withStyles(styles)(({classes, ...props}) => (
+    <AppBar {...props}>
+        <Typography
+            variant="title"
+            color="inherit"
+            className={classes.title}
+            id="react-admin-title"
+        />
+
+        <span className={classes.spacer}/>
+        <UserData/>
+    </AppBar>
+));
+
+const MyLayout = (props) => <Layout {...props} appBar={MyAppBar}/>;
+
+
+export default MyLayout;
