5ac231db855e90e843c18ef549f0004cf6dd5c7b
Nikolay Mikhaylov <sonny@milton.pro>
2020-12-13 14:57:24 +0300

    Add fixture

--- backend/src/DataFixtures/AppFixtures.php
+++ backend/src/DataFixtures/AppFixtures.php
@@ -2,16 +2,95 @@
 
 namespace App\DataFixtures;
 
+use App\DTO\RegisterUserDTO;
+use App\Entity\Account\AbstractAccount;
+use App\Entity\Account\ServiceAccount;
+use App\Entity\User;
+use App\Transaction\TransactionManager;
+use App\User\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+    private UserFactory $userFactory;
+
+    private Generator $faker;
+
+    private TransactionManager $transactionManager;
+
+    /**
+     * AppFixtures constructor.
+     *
+     * @param \App\User\UserFactory               $userFactory
+     * @param \App\Transaction\TransactionManager $transactionManager
+     */
+    public function __construct(UserFactory $userFactory, TransactionManager $transactionManager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->userFactory        = $userFactory;
+        $this->transactionManager = $transactionManager;
+        $this->faker              = Factory::create('en');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function load(ObjectManager $manager): void
+    {
+        $manager->persist($this->createServiceAccountForWelcomeBonus());
+        $manager->flush();
+
+        $users = [];
+
+        for ($i = 0; $i < 50; $i++) {
+            $user = $this->createUser();
+            $manager->persist($user);
+            $users [] = $user;
+        }
+
+        $accounts = array_map(fn(User $user) => $user->getAccount(), $users);
+
+        for ($i = 0; $i < 1000; $i++) {
+            [$from, $to] = $this->faker->randomElements($accounts, 2);
+
+            $fromBalance = $from->getBalance();
+
+            if ($fromBalance === 0) {
+                continue;
+            }
+
+            $amount = $this->faker->numberBetween(1, $fromBalance);
+
+            $this->transactionManager->makeTransaction($from, $to, $amount);
+        }
 
         $manager->flush();
+    }
+
+    /**
+     * @return \App\Entity\Account\AbstractAccount
+     */
+    private function createServiceAccountForWelcomeBonus(): AbstractAccount
+    {
+        $account = new ServiceAccount();
+        $account->setTag('welcome_bonus');
+
+        return $account;
+    }
+
+    /**
+     * @return \App\Entity\User
+     */
+    private function createUser(): User
+    {
+        $registerUserDTO = new RegisterUserDTO(
+            $this->faker->name,
+            $this->faker->email,
+            $this->faker->password
+        );
+
+        return $this->userFactory->createUserFromRegisterDTO($registerUserDTO);
     }
 }
